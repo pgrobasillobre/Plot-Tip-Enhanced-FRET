308d53fc5bcdd2a479a9a99b1d3420a8deba5591
Pablo Grobas Illobre <pgrobasillobre@gmail.com>
2025-06-06 16:03:21 +0200

    simulation results updated

--- simulation/fret/send_all.sh
+++
@@ -1,1 +0,0 @@
-for i in D_sta*; do cd $i; for j in pos-5 pos-6; do cd $j ; qsub run.sh; cd ..; done ; cd ..; done
--- simulation/tddft/pt-pc/state-2/pos-5/change_name_pt-pc.sh
+++
@@ -1,4 +0,0 @@
-mv *Fitdensity_1.cub pt-pc_trans_dens_1.cube
-mv *Fitdensity_2.cub pt-pc_trans_dens_2.cube
-
-rm -rf generated_cube* logfile*
